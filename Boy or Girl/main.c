91cfa80da15960ca3fb71efd8a301da50704a4c9
mehrdad khojastefar <mehrdadkh.uni@gmail.com>
2022-05-20 12:03:24 +0430

    solving boy or girl problem

---
+++ Boy or Girl/main.c
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/236/A
+#include <stdio.h>
+
+int check_gender(char *);
+int count_distinct_characters(char *);
+int count_character(char *, char);
+char *get_distinct_characters(char *);
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
+
+int count_character(char *string, char c)
+{
+    int counter = 0;
+
+    return counter;
+}
+
+int count_distinct_characters(char *username)
+{
+    int counter = strlen(username);
+    for (char *pointer = username; *pointer != '\0'; pointer++)
+    {
+    }
+}
