e4503e469dfde8c40b988da27ab348a28b06c649
mehrdad khojastefar <mehrdadkh.uni@gmail.com>
2022-05-23 14:15:21 +0430

    cleaned up the code

--- CSES_Problemset/wierd algorithm/main.cpp
+++ CSES_Problemset/wierd algorithm/main.cpp
@@ -4,14 +4,15 @@ int main()
 {
     long long int n;
     cin >> n;
-    while(n!=1)
+    while(n>=1)
     {
         cout << n << " ";
+        if (n==1)
+            return 0;
         if ( !(n%2) )
             n /= 2;
         else
             n = (n*3) + 1;
     }
-    cout << 1;
     return 0;
 }
